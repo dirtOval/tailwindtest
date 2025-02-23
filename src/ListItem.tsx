0378eb2dc6ae7b1d9d0467c4c89ca16c8ce54470
deirdre <deirdre.loyall@gmail.com>
2025-02-23 17:24:52 -0500

    used odd: modifier to make every other ListItem have a grey bg. also used nth modifier to make the third one yellow

--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,6 +1,6 @@
 function ListItem({itemNum}) {
   return (
-    <div className="last:bg-yellow-200">
+    <div className="nth-3:bg-yellow-500 odd:bg-gray-400 hover:outline-2 hover:outline-green-500">
       <p>Item Number {itemNum}</p>
     </div>
   )
